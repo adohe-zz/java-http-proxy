c035024a0b2c88aa7f7b9105900ef0ec00e64478
TonyAdo <coolhzb@163.com>
2014-07-22 13:06:51 +0800

    Log.java is done....

--- src/main/java/com/westudio/java/util/Log.java
+++ src/main/java/com/westudio/java/util/Log.java
@@ -48,62 +48,62 @@ public class Log {
     }
 
     public static void v(String s) {
-
+        log(Level.FINE, s, null);
     }
 
     public static void v(Throwable t) {
-
+        log(Level.FINE, "", t);
     }
 
     public static void v(String s, Throwable t) {
-
+        log(Level.FINE, s, t);
     }
 
     public static void d(String s) {
-
+        log(Level.CONFIG, s, null);
     }
 
     public static void d(Throwable t) {
-
+        log(Level.CONFIG, "", t);
     }
 
     public static void d(String s, Throwable t) {
-
+        log(Level.CONFIG, s, t);
     }
 
     public static void i(String s) {
-
+        log(Level.INFO, s, null);
     }
 
     public static void i(Throwable t) {
-
+        log(Level.INFO, "", t);
     }
 
     public static void i(String s, Throwable t) {
-
+        log(Level.INFO, s, t);
     }
 
     public static void w(String s) {
-
+        log(Level.WARNING, s, null);
     }
 
     public static void w(Throwable t) {
-
+        log(Level.WARNING, "", t);
     }
 
     public static void w(String s, Throwable t) {
-
+        log(Level.WARNING, s, t);
     }
 
     public static void e(String s) {
-
+        log(Level.SEVERE, s, null);
     }
 
     public static void e(Throwable t) {
-
+        log(Level.SEVERE, "", t);
     }
 
     public static void e(String s, Throwable t) {
-
+        log(Level.SEVERE, s, t);
     }
 }
